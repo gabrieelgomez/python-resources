e3b07a112b5011a2c6b59b9f8648c3d1703edf4a
gabrieelgomez <gagg1707@gmail.com>
2020-03-19 17:07:54 -0400

    first commit

---
+++ GetterSetter.py
@@ -0,0 +1,24 @@
+import pdb;
+class VotingBox:
+
+    def __init__(self, id, cities_availables):
+        self._id = id
+        self._cities_availables = cities_availables
+        self._region = None
+    
+    @property
+    def region(self):
+        return self._region
+    
+    @region.setter
+    def region (self, region):
+        if region in self._cities_availables:
+            self._region = region
+        else:
+            raise ValueError(f'Region {region}, is not valid in {self._cities_availables}')
+
+if __name__ == '__main__':
+    box = VotingBox(99, ['New York', 'Madrid'])
+    print(box.region)
+    box.region = 'Valencia'
+    print(box.region)
